ff35c8054684df1a8d6c498dd87a0ea5777e289e
Shawn Taylor <shawncurtistaylor@hotmail.com>
2020-04-11 09:24:07 -0700

    clean up

--- Program.cs
+++ Program.cs
@@ -33,15 +33,33 @@ namespace alg200_exercises
             Buzz n = new Buzz(a);
             Console.WriteLine("The factorial of " + a + " is : {0}", n.factorial(a));
 
-            Console.WriteLine(n.factorial1(a));
+            //Console.WriteLine(n.factorial1(a));
+
+
+            for (int i = 1; i <= a; i++)
+            {  
+                if (i % 3 == 0 && i % 5 == 0)
+                {  
+                Console.WriteLine("FizzBuzz");  
+                }    
+                else if (i % 3 == 0)
+                {  
+                Console.WriteLine("Fizz");  
+                }
+                else if (i % 5 == 0)
+                {  
+                Console.WriteLine("Buzz");  
+                }
+                else
+                {  
+                Console.WriteLine(i);  
+            }  
 
 
 
 
-
-
-
-            Console.Read();
         }
+            Console.Read();
     }
+}
 }
