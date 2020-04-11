cad6c9afb162f40cce7e29c96f80c86010a92e21
Shawn Taylor <shawncurtistaylor@hotmail.com>
2020-04-11 03:00:39 -0700

    initial files

---
+++ Fizz-Buzz-Bang.cs
@@ -0,0 +1,13 @@
+//using System;
+
+//namespace alg200_exercises
+//{
+//	class Buzz
+//	{
+//		static int fizzBuzzBang(string[] args)
+//		{
+//			//int count = 0;
+//			Console.WriteLine("Hello World!");
+//		}
+//	}
+//}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace alg200_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = false;
+            int x = 0;
+
+            while (!repeat)
+            {
+            Console.Write("What is your number?  ");
+            bool isNumber = int.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine();
+                if (isNumber)
+                {
+                Console.WriteLine("Thank you for the input!");
+                    repeat = true;
+                }
+                    if (!isNumber)
+                    {
+                    Console.WriteLine("Please enter a valid number!");
+                    repeat = false;
+                    }
+            }
+            Console.WriteLine(x);
+
+
+            Console.Read();
+        }
+    }
+}
